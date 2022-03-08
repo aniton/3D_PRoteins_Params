13e855b3b3e4e2c85ea6256407e30f505c65f55e
Anita Soloveva <34210993+aniton@users.noreply.github.com>
2022-03-08 23:40:47 +0300

    upd

--- src/utils.py
+++ src/utils.py
@@ -14,3 +14,17 @@ def merge_two_dicts(x, y):
     z = x.copy()
     z.update(y)
     return z
+
+
+def split_dict(dict1):
+    k = []
+    dict2 = {}
+    keys = dict1.keys()
+    for key in keys:
+        if key == "ray_trace_gain" or key == "ray_trace_mode":
+            dict2[key] = dict1[key]
+            k.append(key)
+
+    for key in k:
+        del dict1[key]
+    return dict1, dict2
